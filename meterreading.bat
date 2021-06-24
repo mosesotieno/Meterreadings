e87cdad4bdcb795a6ea8db3b0d56e38c22032d03
Moses Otieno <mosotieno25@gmail.com>
2021-06-24 23:44:50 +0300

    Very slight changes to the Shiny app

---
+++ meterreading.bat
@@ -0,0 +1,39 @@
+:: ----------------------------- Begin Header -----------------------------------
+:: Name of the batch file : meterreading.bat
+::
+:: Purpose : Run the insert_readings.py and 01_importdata.R
+::           This script will automate entry of meter readings
+::           on a daily basis and update the dataset appropriately
+::
+:: Input  :  insert_readings.py
+::           01_importdata.R
+::
+:: Output : Updated MySQL database and updated dataset
+::
+::
+:: Authors : Moses Otieno
+::
+::
+:: Contact Email : motieno@kemricdc.org
+::
+::
+:: First version : 24 June 2021
+::
+::
+:: Reviewed :
+::
+:: ----------------------------- End Header--------------------------------------
+
+
+:: Change the directory appropriately
+
+D:
+cd "D:\InterestingTasks\meterreadings"
+
+
+:: Run the python script to enter meter readings
+python "D:\InterestingTasks\meterreadings\insert_readings.py"
+
+:: Run the R script to update the datasets
+
+START "" "C:\Program Files\R\R-4.0.5\bin\x64\Rscript.exe" 909090backup.R /b
