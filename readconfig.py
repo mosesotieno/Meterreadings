65ba7244c1657274825f8cdb29b3ace3df53b559
Moses Otieno <mosotieno25@gmail.com>
2021-06-24 19:38:43 +0300

    Created a script that reads the configuration file

---
+++ readconfig.py
@@ -0,0 +1,11 @@
+# Name: readconfig.py
+
+# Purpose: This script reads the configurations that have been
+#          set in the python_mysql_dbconfig.py script
+
+# Date: 24/06/2021
+
+# Author: Moses Otieno
+
+from python_mysql_dbconfig import read_db_config
+read_db_config()
